89d67d0b91decd3223bceafdd9464f636e410293
Avinash Patel <avipatel30092004@gmail.com>
2025-09-17 22:31:47 +0530

    Config and make Cloudinary utils

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.user(express.static("public"));
+app.use(express.static("public"));
 app.use(cookieParser());
 
 // Import Routes
